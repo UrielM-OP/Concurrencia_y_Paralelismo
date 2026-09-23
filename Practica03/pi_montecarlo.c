f46b15c5dbc26f20487f6dbe4eb7006c4ffe2691
jes <al075457@uacam.mx>
2026-09-23 17:21:43 -0600

    Agrega semaforo, hilos configurables y medicion de tiempo

--- Practica03/pi_montecarlo.c
+++ Practica03/pi_montecarlo.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <time.h>
 
-#define TOTAL_PUNTOS 1000000
-#define NUM_HILOS 4
-
-int totalPuntos = TOTAL_PUNTOS;
-int numHilos = NUM_HILOS;
+long totalPuntos = 1000000;
+int numHilos = 4;
 long puntosDentro = 0; // COMPARTIDO
+sem_t sem;
 
 typedef struct {
     long puntosPorHilo;
@@ -24,20 +23,30 @@ void *generar_puntos(void *arg) {
         double y = ((double)rand_r(&seed) / RAND_MAX) * 2.0 - 1.0;
 
         if (x * x + y * y <= 1.0) {
-            puntosDentro++; // acceso SIN proteger
+            sem_wait(&sem);
+            puntosDentro++;          // seccion critica
+            sem_post(&sem);
         }
     }
     return NULL;
 }
 
-int main(void) {
-    pthread_t hilos[NUM_HILOS];
-    ThreadArg args[NUM_HILOS];
+int main(int argc, char *argv[]) {
+    if (argc >= 2) numHilos = atoi(argv[1]);
+    if (argc >= 3) totalPuntos = atol(argv[2]);
+
+    pthread_t *hilos = malloc(sizeof(pthread_t) * numHilos);
+    ThreadArg *args = malloc(sizeof(ThreadArg) * numHilos);
     long puntosPorHilo = totalPuntos / numHilos;
+
+    sem_init(&sem, 0, 1);
+
+    struct timespec inicio, fin;
+    clock_gettime(CLOCK_MONOTONIC, &inicio);
 
     for (int i = 0; i < numHilos; i++) {
         args[i].puntosPorHilo = puntosPorHilo;
-        args[i].seed = (unsigned int)time(NULL) + i;
+        args[i].seed = (unsigned int)time(NULL) + i * 7919;
         pthread_create(&hilos[i], NULL, generar_puntos, &args[i]);
     }
 
@@ -45,9 +54,18 @@ int main(void) {
         pthread_join(hilos[i], NULL);
     }
 
+    clock_gettime(CLOCK_MONOTONIC, &fin);
+    double segundos = (fin.tv_sec - inicio.tv_sec) +
+                       (fin.tv_nsec - inicio.tv_nsec) / 1e9;
+
     double pi = 4.0 * (double)puntosDentro / (double)totalPuntos;
-    printf("puntosDentro = %ld\n", puntosDentro);
-    printf("pi aproximado = %f\n", pi);
+
+    printf("numHilos=%d totalPuntos=%ld puntosDentro=%ld pi=%f tiempo=%f\n",
+           numHilos, totalPuntos, puntosDentro, pi, segundos);
+
+    sem_destroy(&sem);
+    free(hilos);
+    free(args);
 
     return 0;
 }
